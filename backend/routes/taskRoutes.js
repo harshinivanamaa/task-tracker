70c938e60fde965a5738d346eab740a01ed9c320
harshinivanamaa <harshini.vanamaa12@gmail.com>
2026-05-15 18:42:12 +0530

    fixed undp route order

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -16,8 +16,8 @@ router.get("/", async (req, res) => {
   }
 
   res.json(data);
-});
 
+});
 
 /* 🗑 GET DELETED TASKS */
 router.get("/deleted", async (req, res) => {
@@ -33,8 +33,8 @@ router.get("/deleted", async (req, res) => {
   }
 
   res.json(data);
-});
 
+});
 
 /* 🔹 ADD TASK */
 router.post("/add", async (req, res) => {
@@ -58,8 +58,8 @@ router.post("/add", async (req, res) => {
   }
 
   res.json(data);
-});
 
+});
 
 /* 🗑 MOVE TASK TO TRASH */
 router.delete("/:id", async (req, res) => {
@@ -68,7 +68,9 @@ router.delete("/:id", async (req, res) => {
 
   const { data, error } = await supabase
     .from("tasks")
-    .update({ is_deleted: true })
+    .update({
+      is_deleted: true
+    })
     .eq("id", id)
     .select();
 
@@ -80,8 +82,8 @@ router.delete("/:id", async (req, res) => {
     message: "Moved to Trash",
     data
   });
-});
 
+});
 
 /* ♻️ RESTORE TASK */
 router.put("/restore/:id", async (req, res) => {
@@ -90,7 +92,9 @@ router.put("/restore/:id", async (req, res) => {
 
   const { data, error } = await supabase
     .from("tasks")
-    .update({ is_deleted: false })
+    .update({
+      is_deleted: false
+    })
     .eq("id", id)
     .select();
 
@@ -102,30 +106,10 @@ router.put("/restore/:id", async (req, res) => {
     message: "Task Restored",
     data
   });
-});
 
-
-/* ❌ DELETE FOREVER */
-router.delete("/permanent/:id", async (req, res) => {
-
-  const { id } = req.params;
-
-  const { error } = await supabase
-    .from("tasks")
-    .delete()
-    .eq("id", id);
-
-  if (error) {
-    return res.status(400).json(error);
-  }
-
-  res.json({
-    message: "Deleted Permanently"
-  });
 });
 
 /* ↩ MOVE COMPLETED TO PENDING */
-
 router.put("/undo/:id", async (req, res) => {
 
   const { id } = req.params;
@@ -144,6 +128,26 @@ router.put("/undo/:id", async (req, res) => {
   }
 
   res.json(data);
+
+});
+
+/* ❌ DELETE FOREVER */
+router.delete("/permanent/:id", async (req, res) => {
+
+  const { id } = req.params;
+
+  const { error } = await supabase
+    .from("tasks")
+    .delete()
+    .eq("id", id);
+
+  if (error) {
+    return res.status(400).json(error);
+  }
+
+  res.json({
+    message: "Deleted Permanently"
+  });
 
 });
 
@@ -166,6 +170,7 @@ router.put("/:id", async (req, res) => {
   }
 
   res.json(data);
+
 });
 
 module.exports = router;
